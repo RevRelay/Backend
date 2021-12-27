d4f41fd2a46f63c8a2c9d199a00f6539aa61574f
rphaynes <ryan.haynes@revature.net>
2021-12-27 17:08:33 -0500

    innitgit add .git add .git add .git add .git add .git add .git add .git add .

---
+++ src/main/java/com/revature/RevRelay/RevRelayApplication.java
@@ -0,0 +1,13 @@
+package com.revature.RevRelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevRelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevRelayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/RevRelay/RevRelayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.RevRelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevRelayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
