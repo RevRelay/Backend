2220afa9d8510442740d27900ca26ac3712b208d
Aaron <aaron.litton18@gmail.com>
2021-12-28 14:47:40 -0500

    Added UserRepository, began adding tests for UserRepo

---
+++ src/test/java/com/revature/RevRelay/repositories/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.revature.RevRelay.repositories;
+
+import org.junit.runner.RunWith;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+class UserRepositoryTest {
+
+}
