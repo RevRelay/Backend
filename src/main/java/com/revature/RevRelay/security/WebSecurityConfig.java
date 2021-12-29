8302d9c6b20949b2513561dcf675b5a17f03ec30
Nathan Luther <nathan.luther@revature.net>
2021-12-29 11:57:15 -0500

    documented RevRelayConfig and WebSecurityConfig

--- src/main/java/com/revature/RevRelay/security/WebSecurityConfig.java
+++ src/main/java/com/revature/RevRelay/security/WebSecurityConfig.java
@@ -30,6 +30,12 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * Builds a RequestMatcher for PUBLIC_URLS, defined as URLs that are accessible without authorization.
+     * Included is an attempt at allowing Swagger to pass without authentication, which was unsuccessful at
+     * the time of writing this documentation (NL 211229).
+     */
     private static final RequestMatcher PUBLIC_URLS = new OrRequestMatcher(
             //this allows public pages including user creation and login
             new AntPathRequestMatcher("/public/**")
@@ -43,30 +49,58 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //            new AntPathRequestMatcher( "/swagger-resources/configuration/ui"),
 //            new AntPathRequestMatcher( "/swagger-ui.html")
     );
+
+    /**
+     * Builds a RequestMatcher for PROTECTED_URLS by negation of the list of PUBLIC_URLS.
+     */
     private static final RequestMatcher PROTECTED_URLS = new NegatedRequestMatcher(PUBLIC_URLS);
 
-    @Autowired
     TokenAuthProvider provider;
-    @Autowired
     PasswordEncoder passwordEncoder;
-    @Autowired
     UserDetailsService userDetailsService;
 
-    WebSecurityConfig(final TokenAuthProvider provider) {
+    /**
+     * Autowired constructor providing TokenAuthProvider, PasswordEncoder, and UserDetailsService.
+     *
+     * @param provider           TokenAuthProvider from class in the security package.
+     * @param passwordEncoder    PasswordEncoder from RevRelayConfig; new BCryptPasswordEncoder().
+     * @param userDetailsService UserDetailsService from UserService.
+     */
+    @Autowired
+    WebSecurityConfig(TokenAuthProvider provider, PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
         super();
         this.provider = requireNonNull(provider);
+        this.passwordEncoder = requireNonNull(passwordEncoder);
+        this.userDetailsService = requireNonNull(userDetailsService);
     }
 
+    /**
+     * configure override to use TokenAuthProvider in configuration.
+     *
+     * @param auth AuthenticationManagerBuilder configured by writing.
+     */
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(provider);
     }
 
+    /**
+     * configure override for WebSecurity. At this time, configures only PUBLIC_URLS.
+     *
+     * @param web WebSecurity configured by wiring.
+     */
     @Override
     public void configure(final WebSecurity web) {
         web.ignoring().requestMatchers(PUBLIC_URLS);
     }
 
+    /**
+     * Configures HttpSecurity; this is where the magic happens. Many of these elements add default or disabled
+     * configuration elements, which are largely self-explanatory.
+     *
+     * @param http Provided/wired HttpSecurity object.
+     * @throws Exception Possible untyped exceptions thrown by .cors() and restAuthenticationFilter() elements.
+     */
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http
@@ -91,6 +125,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout().disable();
     }
 
+    /**
+     * Provides default CorsConfigurationSource using applyPermitDefaultValues().
+     *
+     * @return CorsConfigurationSource configured with applyPermitDefaultValues on all endpoints.
+     */
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -98,6 +137,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return source;
     }
 
+//    Alternate bean for providing CorsConfigurationSource. Necessary for allowing non-default verbs such as delete.
+//    As the defaults are plenty, this should not be used, but I'm keeping it just in case we need it. - NL
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("*"));
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+//        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+//        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+
+    /**
+     * Provides a TokenAuthFilter configured on PROTECTED_URLS using authenticationManager() and successHandler().
+     *
+     * @return Configured TokenAuthFilter.
+     * @throws Exception Catches untyped Exceptions thrown by authenticationManager().
+     */
     @Bean
     TokenAuthFilter restAuthenticationFilter() throws Exception {
         final TokenAuthFilter filter = new TokenAuthFilter(PROTECTED_URLS);
@@ -106,6 +165,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return filter;
     }
 
+    /**
+     * Provides a SimpleUrlAuthenticationSuccessHandler containing an empty NoRedirectStrategy().
+     *
+     * @return SuccessHandler as above.
+     */
     @Bean
     SimpleUrlAuthenticationSuccessHandler successHandler() {
         final SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
@@ -123,25 +187,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return registration;
     }
 
+    /**
+     * Creates a bean establishing forbidden endpoints as forbidden.
+     *
+     * @return AuthenticationEntryPoint defining forbidden endpoints.
+     */
     @Bean
     AuthenticationEntryPoint forbiddenEntryPoint() {
         return new HttpStatusEntryPoint(FORBIDDEN);
     }
-
-//    Necessary for allowing non-default verbs such as delete - NL
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("*"));
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-//        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-//        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-
-
 
 //    provided by tutorial, may be unnecessary? (possibly redundant to TokenAuthProvider).
 //    @Bean @Override
