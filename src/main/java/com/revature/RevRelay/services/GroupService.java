92acf3fbc017f340a7473a7ccb1d183e43518a4b
nfrederick023 <nfrederick023@gmail.com>
2021-12-29 14:03:29 -0500

    Groups Documentation
    Added documentation for the service, model, repository, and controller methods and classes for the group model.

--- src/main/java/com/revature/RevRelay/services/GroupService.java
+++ src/main/java/com/revature/RevRelay/services/GroupService.java
@@ -8,48 +8,130 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service layer for Group model
+ * 
+ * @author Isaiah Anason
+ * @author Noah Frederick
+ * @author Loustauanau Luis
+ * @author Evan Ritchey
+ * @author Ryan Haynes
+ */
+
 @NoArgsConstructor
 @Service
 public class GroupService {
 
     GroupRepository groupRepository;
 
+    /**
+     * Constructor for GroupService
+     * 
+     * @param groupRepository is the data layer for the Group model
+     */
     @Autowired
-    public GroupService(GroupRepository groupRepository){
+    public GroupService(GroupRepository groupRepository) {
         this.groupRepository = groupRepository;
     }
 
-    //CREATE
+    /**
+     * Persists a new Group to the database
+     * 
+     * Recieves a Group object from the controller and persists it as a new Group
+     * record on the database
+     * 
+     * @param group is the Group object to be persisted
+     * @return Group is the Group created on the database
+     */
     public Group createGroup(Group group) {
         return groupRepository.save(group);
     }
 
-    //READ
+    /**
+     * Retrieves all Groups from the database
+     * 
+     * @return Page<Group> is a page containing all the Groups
+     */
     public Page<Group> getAll() {
         return groupRepository.findAll(Pageable.unpaged());
     }
+
+    /**
+     * Retrieves all Groups from the database
+     * 
+     * Receives a pageable, that specifies a specific page to return from
+     * Page<Group>
+     * 
+     * @param pageable is the specific page to return from the Page<Group>
+     * @return Page<Group> is page containing the Groups from a specific page
+     */
     public Page<Group> getAll(Pageable pageable) {
         return groupRepository.findAll(pageable);
     }
 
-    public Page<Group> findAllByUserOwnerID(Integer userOwnerID, Pageable pageable){
+    /**
+     * Retrieves all Groups from the database by their OwnerID
+     * 
+     * Recieves a OwnerID from the controller and queries the database for
+     * every Group which has that OwnerID
+     * 
+     * @param userOwnerID is the ID of the owner used when querying the database
+     * @return Page<Group> is page containing the Groups
+     */
+    public Page<Group> findAllByUserOwnerID(Integer userOwnerID) {
+        return groupRepository.findGroupsByUserOwnerID(userOwnerID, Pageable.unpaged());
+    }
+
+    /**
+     * Retrieves all Groups from the database by their OwnerID
+     * 
+     * Recieves a OwnerID from the controller and queries the database for
+     * every Group which has that OwnerID. Also receives a pageable, that specifies
+     * a specific page to return from Page<Group>
+     * 
+     * @param userOwnerID is the ID of the owner used when querying the database
+     * @param pageable    is the specific page to return from the Page<Group>
+     * @return Page<Group> is page containing the Groups from a specific page
+     */
+    public Page<Group> findAllByUserOwnerID(Integer userOwnerID, Pageable pageable) {
         return groupRepository.findGroupsByUserOwnerID(userOwnerID, pageable);
     }
-    public Page<Group> findAllByUserOwnerID(Integer userOwnerID){
-        return groupRepository.findGroupsByUserOwnerID(userOwnerID,Pageable.unpaged());
-    }
 
+    /**
+     * Retrieves all Groups from the database by their ID
+     * 
+     * Recieves a Group ID from the controller and queries the database for
+     * that group
+     * 
+     * @param groupID
+     * @return Group
+     */
     public Group getGroupByGroupID(Integer groupID) {
         return groupRepository.getGroupsByGroupID(groupID).orElse(null);
-        //TODO Custom exception
+        // TODO Custom exception
     }
 
-    //UPDATE
+    /**
+     * Updates a Group
+     * 
+     * Recieves a Group object from the controller and updates that Group on the
+     * database
+     * 
+     * @param group is the Group object to be persisted
+     * @return Group is the Group created on the database
+     */
     public Group updateGroups(Group group) {
         return groupRepository.save(group);
     }
 
-    //DELETE
+    /**
+     * Deletes a Group
+     * 
+     * Recieves a Group ID from the controller and deletes the Group with that ID
+     * from the database
+     * 
+     * @param groupID
+     */
     public void deleteGroupsByID(Integer groupID) {
         groupRepository.deleteById(groupID);
     }
