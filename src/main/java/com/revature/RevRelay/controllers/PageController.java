0ea58de78cb8f10eeebfbc1c498d321330c23c73
JennicaLeClerc <93157746+JennicaLeClerc@users.noreply.github.com>
2021-12-29 12:50:04 -0500

    Update PageController.java

--- src/main/java/com/revature/RevRelay/controllers/PageController.java
+++ src/main/java/com/revature/RevRelay/controllers/PageController.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @NoArgsConstructor
