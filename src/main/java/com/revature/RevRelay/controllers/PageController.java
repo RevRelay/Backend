9619d499ac74e2f9488715aeb8621f0f8f330d21
Noah Frederick <nfrederick023@gmail.com>
2021-12-29 14:55:32 -0500

    Fix Page JavaDocs V.1

--- src/main/java/com/revature/RevRelay/controllers/PageController.java
+++ src/main/java/com/revature/RevRelay/controllers/PageController.java
@@ -8,84 +8,86 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+/**
+* This controller handles pages CRUD functionality
+*/
 @NoArgsConstructor
 @RestController
 @RequestMapping(value = "/pages")
-/**
- * This controller handles pages CRUD functionality
- */
 public class PageController {
     PageService pageService;
 
-    @Autowired
     /**
-     * @param pageService autowired pageService
-     */
+    * @param pageService autowired pageService
+    */
+    @Autowired
     public PageController(PageService pageService) {
         this.pageService = pageService;
     }
 
     //CREATE
-    @PostMapping
+    
     /**
-     * @param Page page to be created
-     * @return ResponseEntity<Page> Returns the created Page is successful
-     */
+    * @param Page page to be created
+    * @return ResponseEntity<Page> Returns the created Page is successful
+    */
+    @PostMapping
     public ResponseEntity<Page> createPage(@RequestBody Page page) {
         return ResponseEntity.ok(pageService.createPage(page));
     }
 
     //READ
-    @GetMapping("/all")
+    
     /**
-     * @return List<Page>
-     */
+    * @return List<Page>
+    */
+    @GetMapping("/all")
     public List<Page> getAll() {
         return pageService.getAll();
     }
 
-    @GetMapping("/users/{userOwnerID}")
     /**
-     * @param userOwnerID userOwnerID to be searched and returned
-     * @return Page single page matching provided userOwnerID
-     */
+    * @param userOwnerID userOwnerID to be searched and returned
+    * @return Page single page matching provided userOwnerID
+    */
+    @GetMapping("/users/{userOwnerID}")
     public Page getPageByUserOwnerID(@PathVariable Integer userOwnerID) {
         return pageService.getPageByUserOwnerID(userOwnerID);
     }
 
-    @GetMapping("/groups/{groupID}")
     /**
-     * @param groupOwnerID groupOwnerID to be searched and returned
-     * @return Page single page matching provided groupID
-     */
+    * @param groupOwnerID groupOwnerID to be searched and returned
+    * @return Page single page matching provided groupID
+    */
+    @GetMapping("/groups/{groupID}")
     public Page getPageByGroupOwnerID(@PathVariable Integer groupID) {
         return pageService.getPageByGroupID(groupID);
     }
 
-    @GetMapping("/{pageID}")
     /**
-     * @param pageID pageID to be searched and returned
-     * @return Page single page matching provided pageID
-     */
+    * @param pageID pageID to be searched and returned
+    * @return Page single page matching provided pageID
+    */
+    @GetMapping("/{pageID}")
     public Page getPageByPageID(@PathVariable Integer pageID) {
         return pageService.getPageByPageID(pageID);
     }
 
     //UPDATE
-    @PutMapping
     /**
-     * @param page the page to be updated
-     * @return Page single page with the updated values
-     */
+    * @param page the page to be updated
+    * @return Page single page with the updated values
+    */
+    @PutMapping
     public Page updatePage(@RequestBody Page page) {
         return pageService.updatePage(page);
     }
 
     //DELETE
-    @DeleteMapping("/{pageID}")
     /**
-     * @param pageID the pageID to be deleted from database
-     */
+    * @param pageID the pageID to be deleted from database
+    */
+    @DeleteMapping("/{pageID}")
     public void deletePageByID(@PathVariable Integer pageID) {
         pageService.deletePageByID(pageID);
     }
