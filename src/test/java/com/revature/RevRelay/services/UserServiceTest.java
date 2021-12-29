4ec59f98475cf52b0a2ec5b5eb38fa6a1e882f89
Nathan <43795121+LutherNa@users.noreply.github.com>
2021-12-29 13:30:41 -0500

    changed assert import
    from
    org.junit.Assert.assertTrue
    to
    org.junit.jupiter.api.Assertions.assertTrue

--- src/test/java/com/revature/RevRelay/services/UserServiceTest.java
+++ src/test/java/com/revature/RevRelay/services/UserServiceTest.java
@@ -16,8 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.util.ReflectionTestUtils;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
