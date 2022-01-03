ca7f88f88a1f6dc4c96fbee5c369a734534e1d0e
luisloustaunau <luis62civil@gmail.com>
2022-01-03 16:50:36 -0600

    more integration test for groups

--- src/test/java/com/revature/RevRelay/services/controllerTest/GroupControllerTest.java
+++ src/test/java/com/revature/RevRelay/services/controllerTest/GroupControllerTest.java
@@ -3,11 +3,14 @@ package com.revature.RevRelay.services.controllerTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.RevRelay.controllers.GroupsController;
 import com.revature.RevRelay.controllers.UserController;
+import com.revature.RevRelay.enums.PostType;
 import com.revature.RevRelay.models.Group;
+import com.revature.RevRelay.models.Post;
 import com.revature.RevRelay.models.User;
 import com.revature.RevRelay.repositories.GroupRepository;
 import com.revature.RevRelay.repositories.UserRepository;
 
+import org.assertj.core.util.DateUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,8 +21,7 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -88,4 +90,34 @@ class GroupControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/groups/{groupsID}", 10)).andExpect(status().isOk())
                 .andDo(print());
     }
+
+    @Test
+    void updateGroupsTestWithValidData () throws Exception {
+        Group group = new Group();
+        User user1 = userRepository.save(user);
+        group.setGroupName("hello");
+        group.setUserOwner(user1);
+        group.setPrivate(false);
+        groupRepository.save(group);
+        mockMvc = MockMvcBuilders.standaloneSetup(groupsController).build();
+        mockMvc.perform(MockMvcRequestBuilders.put("/groups")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(group)))
+                .andExpect((status().isOk())).andDo(print());
+    }
+
+    @Test
+    void deleteGroupIDTestWithValidData () throws Exception {
+        Group group = new Group();
+        User user1 = userRepository.save(user);
+        group.setGroupName("hello");
+        group.setUserOwner(user1);
+        group.setPrivate(false);
+        groupRepository.save(group);
+        mockMvc = MockMvcBuilders.standaloneSetup(groupsController).build();
+        mockMvc.perform(MockMvcRequestBuilders.delete("/groups/{groupID}", group.getGroupID()))
+                .andExpect((status().isOk())).andDo(print());
+    }
+
+
 }
