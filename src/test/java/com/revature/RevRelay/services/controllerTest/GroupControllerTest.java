f7358c8bd684c04978e074accaadcb7e30f9c163
JennicaLeClerc <93157746+JennicaLeClerc@users.noreply.github.com>
2022-01-03 17:30:59 -0500

    Update GroupControllerTest.java

--- src/test/java/com/revature/RevRelay/services/controllerTest/GroupControllerTest.java
+++ src/test/java/com/revature/RevRelay/services/controllerTest/GroupControllerTest.java
@@ -7,7 +7,6 @@ import com.revature.RevRelay.models.Group;
 import com.revature.RevRelay.models.User;
 import com.revature.RevRelay.repositories.GroupRepository;
 import com.revature.RevRelay.repositories.UserRepository;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
