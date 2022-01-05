ba53157647f063913b6134e31a8eb533237a7cec
Nathan Luther <nathan.luther@revature.net>
2022-01-04 22:03:35 -0500

    utd with main

--- src/main/java/com/revature/RevRelay/controllers/UserController.java
+++ src/main/java/com/revature/RevRelay/controllers/UserController.java
@@ -43,8 +43,6 @@ public class UserController {
     }
 
 
-
-
     /**
      * Returns a UserDTO representing the logged-in user via JWT.
      *
@@ -70,7 +68,7 @@ public class UserController {
      * @return response entity 200 signaling successful creation
      */
     @GetMapping("/{userID}")
-    public ResponseEntity<?> findByUserID(@PathVariable int userID){
+    public ResponseEntity<?> findByUserID(@PathVariable int userID) {
         return ResponseEntity.ok(userService.loadUserDTOByUserID(userID));
     }
 
@@ -83,7 +81,7 @@ public class UserController {
      */
     @PutMapping("/firstName/{userID}")
     public ResponseEntity<?> updateFirstName(@PathVariable int userID, @RequestBody String firstName) {
-        return ResponseEntity.ok(userService.updateFirstName(userID, firstName.substring(1,firstName.length()-1)));
+        return ResponseEntity.ok(userService.updateFirstName(userID, firstName.substring(1, firstName.length() - 1)));
     }
 
     /**
@@ -95,7 +93,7 @@ public class UserController {
      */
     @PutMapping("/lastName/{userID}")
     public ResponseEntity<?> updateLastName(@PathVariable int userID, @RequestBody String lastName) {
-        return ResponseEntity.ok(userService.updateLastName(userID, lastName.substring(1,lastName.length()-1)));
+        return ResponseEntity.ok(userService.updateLastName(userID, lastName.substring(1, lastName.length() - 1)));
     }
 
     /**
@@ -125,7 +123,7 @@ public class UserController {
      */
     @PutMapping("/displayName/{userID}")
     public ResponseEntity<?> updateDisplayName(@PathVariable int userID, @RequestBody String displayName) {
-        return ResponseEntity.ok(userService.updateDisplayName(userID, displayName.substring(1,displayName.length()-1)));
+        return ResponseEntity.ok(userService.updateDisplayName(userID, displayName.substring(1, displayName.length() - 1)));
     }
 
     /**
@@ -139,8 +137,8 @@ public class UserController {
     public ResponseEntity<?> updateBirthDate(@PathVariable int userID, @RequestBody String birthDate) {
         Date s = null;
         try {
-            s = new SimpleDateFormat("yyyy/MM/dd").parse(birthDate.substring(1,birthDate.length()-1));
-        } catch(Exception e) {
+            s = new SimpleDateFormat("yyyy/MM/dd").parse(birthDate.substring(1, birthDate.length() - 1));
+        } catch (Exception e) {
             return null;
         }
         return ResponseEntity.ok(userService.updateBirthDate(userID, s));
@@ -149,13 +147,13 @@ public class UserController {
     /**
      * Updates a user's email using the userID as the identifier
      *
-     * @param userID      userId of user being updated
-     * @param email user information being changed
+     * @param userID userId of user being updated
+     * @param email  user information being changed
      * @return response entity 200 signaling successful update
      */
     @PutMapping("/email/{userID}")
     public ResponseEntity<?> updateEmail(@PathVariable int userID, @RequestBody String email) {
-        return ResponseEntity.ok(userService.updateEmail(userID, email.substring(1,email.length()-1)));
+        return ResponseEntity.ok(userService.updateEmail(userID, email.substring(1, email.length() - 1)));
     }
 
 }
